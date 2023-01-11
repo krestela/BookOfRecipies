155bb2ad67b20719eca912e0cd7eead7307be4fa
Kristina Adigamova <kriwowak@yandex.ru>
2023-01-11 22:36:15 +0500

    Первый коммит

---
+++ src/main/java/com/example/bookofrecipies/BookOfRecipiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookofrecipies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOfRecipiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookOfRecipiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookofrecipies/BookOfRecipiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookofrecipies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookOfRecipiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
