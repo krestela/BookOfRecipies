14fc83c173f3dfcd881f53a253b62d67f7677f0d
Kristina Adigamova <kriwowak@yandex.ru>
2023-01-19 22:24:29 +0500

    Дз по библиотекам

--- src/main/java/com/example/bookofrecipies/controller/IngredientsController.java
+++ src/main/java/com/example/bookofrecipies/controller/IngredientsController.java
@@ -1,12 +1,20 @@
 package com.example.bookofrecipies.controller;
 
 import com.example.bookofrecipies.model.Ingridients;
+import com.example.bookofrecipies.model.Recipe;
 import com.example.bookofrecipies.service.IngredientsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ingredient")
+@Tag(name = "Ингридиенты", description = "Ингридиенты и методы с ними")
 public class IngredientsController {
     private IngredientsService ingredientsService;
 
@@ -14,12 +22,42 @@ public class IngredientsController {
         this.ingredientsService = ingredientsService;
     }
     @PostMapping("/addingr")
-    public void addIngredient(@RequestParam Ingridients ingridients) {
-        ingredientsService.addNewIngredient(ingridients);
+    @Operation(description = "Добавление нового ингридиента")
+    public ResponseEntity<Long> addIngredient(@RequestBody Ingridients ingridients) {
+        long id = ingredientsService.addNewIngredient(ingridients);
+        return ResponseEntity.ok().body(id);
     }
 
     @GetMapping("/{id}")
-    public void getIngredient(@PathVariable long id) {
+    @Operation(description = "Получения ингридиента по id")
+    public ResponseEntity<Ingridients> getIngredient(@PathVariable long id) {
+        Ingridients ingridients= ingredientsService.getIngredient(id);
+        if (ingridients == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ingridients);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(description = "Редактирование списка ингредиентов")
+    public ResponseEntity<Ingridients> editIngredients(@PathVariable long id, @RequestBody Ingridients ingridients) {
         ingredientsService.getIngredient(id);
+        if (ingridients == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ingridients);
+    }
+    @DeleteMapping("/{id}")
+    @Operation(description = "Удаление ингридиента")
+    public ResponseEntity<Void> deleteIngrediends(@PathVariable long id) {
+        if (ingredientsService.deleteIngrediends(id)){
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @GetMapping("/all")
+    @Operation(description = "Получение списка всех игредиентов")
+    public void getAllIngredient() {
+        ingredientsService.getAllIngredient();
     }
 }
